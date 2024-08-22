26ed1fd8d15c3e08aec9512890d24b24da9c6608
UyenVo1108 <uvo1446@gmail.com>
2024-08-22 22:52:53 +0700

    uyen

--- SPCK_MinhAnh/assets/js/jquery.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-  var giohang = $("#giohang").find(".viewCart-product");
-  var productCount = giohang.length;
-  console.log("Number of products in the cart: " + productCount);
-});
